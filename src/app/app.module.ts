576d810027b3d0b58393348a2fb6db79874ce6f0
Sparshsaxenain <sparshsaxena3649@gmail.com>
2022-07-18 10:53:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EmployeeItemComponent } from './components/employee-item/employee-item.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { AboutComponent } from './component/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeeItemComponent,
+    EmployeesComponent,
+    AddEmployeeComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
